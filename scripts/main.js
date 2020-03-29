26bb81bc1e69f421feb6a59d50e1fddcc4e921cb
bhupendrasinghnegi <bhupendrasinghnegi@users.noreply.github.com>
2020-03-29 21:17:46 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ $(".site-footer").hide();
   $("#btn1").click(function () { 
   console.log("helo0");
   var name = $("#inpName").val();
-    var link = "https://www.naamvidya.com/numerology/name-meaning/"+name;
+    var link = "https://www.naamvidya.com/numerology/name-meaning/"+name+".html";
     console.log(link);
     $("#frame").attr("src", link);
 });
