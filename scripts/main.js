4bd6e2c25ed247f09d6c53314cc0e202596df574
bhupendrasinghnegi <bhupendrasinghnegi@users.noreply.github.com>
2020-03-29 20:57:58 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,16 @@
 $().ready(function() { 
-  $(".page-header").hide();
-   $(".site-footer").hide();
+  //$(".page-header").hide();
+//$(".site-footer").hide();
   
   var name = $("#inpName").val();
   //alert(name);
 });
-
+$(window).on( "load", function() {
+  $(".page-header").hide();
+   $(".site-footer").hide();
+});
+/*
 $().load(function() { 
 $(".page-header").hide();
    $(".site-footer").hide();
-});
+});*/
